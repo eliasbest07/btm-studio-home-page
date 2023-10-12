b5719dae1905dc59de4ed5eb75536a36c801efd2
Elias Montilla <eliasbest634@gmail.com>
2023-10-12 17:09:09 -0400

    hot fix

--- app/page.js
+++ app/page.js
@@ -44,7 +44,9 @@ export default function Home() {
 
         </div>
         <div>
-            <RectangleComponent children={ <h1 style={{ marginLeft:'10px', marginRight:'10px' ,color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Donde algunos vieron dificultad, nosotros vemos oportunidades. Este es el mejor momento para destacar, somos tu aliado estratégico.</h1>} color={'rgba(41, 41, 38, 0.6)'}  left={0} width={560} height={260}/>
+            <RectangleComponent color={'rgba(41, 41, 38, 0.6)'}  left={0} width={560} height={260}>
+            <h1 style={{ marginLeft:'10px', marginRight:'10px' ,color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Donde algunos vieron dificultad, nosotros vemos oportunidades. Este es el mejor momento para destacar, somos tu aliado estratégico.</h1>
+            </RectangleComponent> 
             <div
         style={{ height:'30px' }}
         >
@@ -59,7 +61,9 @@ export default function Home() {
     <br/>
     <br/>
     <div style={{ display: 'flex', justifyContent: 'space-between' }}> 
-    <RectangleComponent children={ <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Te invitamos a explorar nuestros servicios y descubrir cómo podemos trabajar juntos para convertir los desafíos en éxitos.</h1>} color={'rgba(41, 41, 38, 0.6)'}  left={0} width={560} height={318}/>
+    <RectangleComponent  color={'rgba(41, 41, 38, 0.6)'}  left={0} width={560} height={318}>
+    <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Te invitamos a explorar nuestros servicios y descubrir cómo podemos trabajar juntos para convertir los desafíos en éxitos.</h1>
+    </RectangleComponent>
     <div
         style={{ width:'30px' }}
         >
@@ -73,8 +77,9 @@ export default function Home() {
     <div style={{ display: 'flex', justifyContent: 'space-between' }}> 
     <RectangleImg width={285} height={285} img={'/city-into-the-ocean.png'}/>
     <div style={{ width:'30px' }}></div>
-    <RectangleComponent children={ <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Contamos con experiencia creando y manteniendo productos digitales </h1>} color={'rgba(41, 41, 38, 0.6)'}  left={0} width={560} height={285}/>
-
+    <RectangleComponent color={'rgba(41, 41, 38, 0.6)'}  left={0} width={560} height={285}>
+    <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Contamos con experiencia creando y manteniendo productos digitales </h1>
+    </RectangleComponent>
     </div>
     <br/>
     <br/>
@@ -82,7 +87,9 @@ export default function Home() {
     <RectangleImg width={285} height={474} img={'/rainforest.png'}/>
     <div style={{ width:'30px' }}></div>
     <div>
-            <RectangleComponent children={ <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Landing Pages, Apps Web, Apps for Android, Apps for Ios, Apps for desktop </h1>} color={'rgba(21, 38, 56, 0.6)'}  left={0} width={560} height={250}/>
+            <RectangleComponent color={'rgba(21, 38, 56, 0.6)'}  left={0} width={560} height={250}>
+            <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>Landing Pages, Apps Web, Apps for Android, Apps for Ios, Apps for desktop </h1>
+            </RectangleComponent>
             <div
         style={{ height:'30px' }}
         >
@@ -95,7 +102,9 @@ export default function Home() {
     <br/>
     <br/>
     <br/>
-    <RectangleComponent  children={ <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>En construcción </h1>} color={'rgba(158, 158, 149, 0.2)'} left={20} width={460} height={240}/>
+    <RectangleComponent color={'rgba(158, 158, 149, 0.2)'} left={20} width={460} height={240}>
+    <h1 style={{ color: '#57EBFF', textShadow: '2px 2px 4px #000000',textAlign: 'center', fontSize: '1.5em'  }}>En construcción </h1>
+    </RectangleComponent>
     <RectangleComponent color={'rgba(158, 158, 149, 0.2)'} left={20} width={560} height={240}/>
     <RectangleComponent color={'rgba(158, 158, 149, 0.2)'} left={20} width={460} height={440}/>
     <RectangleComponent color={'rgba(158, 158, 149, 0.2)'} left={20} width={560} height={440}/>
